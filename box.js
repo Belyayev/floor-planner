5a69e836e31d5cf85654d69eac2a9e76908b7ab5
Mike Belyayev <4xgood@gmail.com>
2024-11-01 19:15:59 -0600

    Update box.js

--- box.js
+++ box.js
@@ -1,9 +1,9 @@
 export function createBox(scene) {
-    const redMaterial = new BABYLON.StandardMaterial("redMaterial", scene);
-    redMaterial.emissiveColor = new BABYLON.Color3(1, 0, 0);
+    const greenMaterial = new BABYLON.StandardMaterial("greenMaterial", scene);
+    greenMaterial.emissiveColor = new BABYLON.Color3(0, 1, 0); // Change to green
 
     const box = BABYLON.MeshBuilder.CreateBox("box", { size: 2 }, scene);
-    box.material = redMaterial;
+    box.material = greenMaterial;
 
     const glowLayer = new BABYLON.GlowLayer("glow", scene);
     glowLayer.intensity = 0.5;
