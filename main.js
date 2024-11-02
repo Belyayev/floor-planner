c13162ff4cd816b48820f7fcee9e07413c705b02
Mike Belyayev <4xgood@gmail.com>
2024-11-01 18:34:52 -0600

    Update main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+import { createScene } from './scene.js';
+
+window.addEventListener('DOMContentLoaded', () => {
+    const canvas = document.getElementById('renderCanvas');
+    const engine = new BABYLON.Engine(canvas, true);
+
+    const scene = createScene(engine, canvas);
+
+    engine.runRenderLoop(() => {
+        scene.render();
+    });
+
+    window.addEventListener('resize', () => {
+        engine.resize();
+    });
+});
